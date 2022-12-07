52b8e0c4199bad0252bfe10c64c7b3887be45a08
Ruy Diaz <diazruy@gmail.com>
2022-12-07 09:24:07 -0800

    Add answer to Day 4, part 1

--- main.rb
+++ main.rb
@@ -1,5 +1,19 @@
 require './input_reader'
 
-InputReader.each_line do |line|
-  puts line
+def to_range(string)
+  min, max = string.split('-')
+  (min.to_i..max.to_i)
 end
+
+def contained?(range_1, range_2)
+  range_1.cover?(range_2) || range_2.cover?(range_1)
+end
+
+total = 0
+
+InputReader.each_line do |line|
+  ranges = line.split(',').collect { |range_string| to_range(range_string) }
+  total += 1 if contained?(*ranges)
+end
+
+puts total
