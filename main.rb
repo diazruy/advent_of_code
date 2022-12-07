ce44510e48d2059dde32704d18078b1264715713
Ruy Diaz <diazruy@gmail.com>
2022-12-06 23:02:50 -0800

    Add Day 3, Part 1 answer

--- main.rb
+++ main.rb
@@ -1,5 +1,25 @@
+require 'byebug'
 require './input_reader'
 
-InputReader.each_line do |line|
-  puts line
+PRIORITIES = ('a'..'z').to_a + ('A'..'Z').to_a
+
+def priority(item)
+  PRIORITIES.index(item) + 1
 end
+
+total = 0
+
+def compartments(items)
+  items.each_slice(items.length / 2).to_a
+end
+
+def common_item(compartments)
+  compartments.reduce(&:&).first
+end
+
+InputReader.each_line do |line|
+  items = line.split('')
+  total += priority(common_item(compartments(items)))
+end
+
+puts total
