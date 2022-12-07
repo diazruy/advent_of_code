de5236c66681375abec5448d61f32b7304028a91
Ruy Diaz <diazruy@gmail.com>
2022-12-06 22:10:04 -0800

    Day 2, Part 2

--- main.rb
+++ main.rb
@@ -1,44 +1,50 @@
-WIN = 6
-DRAW = 3
-LOSE = 0
-
 ROCK = :rock
 PAPER = :paper
 SCISSORS = :scissors
 
-SCORES_SHAPES = {
+CODE_LOSE = 'X'
+CODE_DRAW = 'Y'
+CODE_WIN = 'Z'
+
+RESULT_POINTS = {
+  CODE_LOSE => 0,
+  CODE_DRAW => 3,
+  CODE_WIN => 6
+}
+
+SHAPE_SCORES = {
   ROCK => 1,
   PAPER => 2,
   SCISSORS => 3
 }
 
-def shape(value)
-  case value
-  when 'A', 'X' then :rock
-  when 'B', 'Y' then :paper
-  when 'C', 'Z' then :scissors
-  end
-end
+SHAPE_CODES = {
+  'A' => ROCK,
+  'B' => PAPER,
+  'C' => SCISSORS
+}
 
-def score_result(them, us)
-  return DRAW if them == us
+OFFSETS = {
+  CODE_LOSE => -1,
+  CODE_DRAW => 0,
+  CODE_WIN => 1,
+}
 
-  case them
-  when ROCK
-    us == PAPER ? WIN : LOSE
-  when PAPER
-    us == SCISSORS ? WIN : LOSE
-  when SCISSORS
-    us == ROCK ? WIN : LOSE
-  end
+SEQUENCE = [ROCK, PAPER, SCISSORS, ROCK, PAPER, SCISSORS]
+
+def shape_for_desired_result(shape_them, result)
+  SEQUENCE[SEQUENCE.index(shape_them) + OFFSETS[result]]
 end
 
 total_score = 0
 
 File.open('input.txt', 'r') do |file|
   file.each do |line|
-    them, us = line.split(' ').map { |value| shape(value) }
-    total_score += score_result(them, us) + SCORES_SHAPES[us]
+    code_them, desired_result = line.split(' ')
+    shape_them = SHAPE_CODES[code_them]
+    shape_us = shape_for_desired_result(shape_them, desired_result)
+    score = RESULT_POINTS[desired_result] + SHAPE_SCORES[shape_us]
+    total_score += score
   end
 end
 puts total_score
