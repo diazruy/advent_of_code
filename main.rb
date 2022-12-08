c1b76042d018814be5919d06e741fe8b7ee1b89d
Ruy Diaz <diazruy@gmail.com>
2022-12-08 08:46:13 -0800

    Add answer to Day 6, part 1

--- main.rb
+++ main.rb
@@ -1,5 +1,18 @@
 require './input_reader'
 
+WINDOW_SIZE = 4
+
+def start_of_packet_marker?(window)
+  window.split('').uniq.length == WINDOW_SIZE
+end
+
 InputReader.each_line do |line|
-  puts line
+  (line.length - WINDOW_SIZE).times do |index|
+    to = index + WINDOW_SIZE
+    window = line[index...to]
+    if start_of_packet_marker?(window)
+      puts to
+      exit
+    end
+  end
 end
