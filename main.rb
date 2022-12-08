3565940f969730fb252f1d74264cee5223919bb4
Ruy Diaz <diazruy@gmail.com>
2022-12-07 23:33:05 -0800

    Add answer to Day 5, part 1

--- main.rb
+++ main.rb
@@ -1,5 +1,43 @@
 require './input_reader'
 
-InputReader.each_line do |line|
-  puts line
+STACK_COUNT = 9
+STACK_LINE_REGEX = /\[/
+MOVE_LINE_REGEX = /\Amove (?<amount>\d+) from (?<from>\d+) to (?<to>\d+)/
+
+stacks = []
+STACK_COUNT.times { stacks << [] }
+
+def stack_line?(line)
+  line =~ STACK_LINE_REGEX
 end
+
+def move_line?(line)
+  line =~ MOVE_LINE_REGEX
+end
+
+def build_stacks(stacks, line)
+  STACK_COUNT.times do |stack_index|
+    item = line[stack_index * 4 + 1].strip
+    stacks[stack_index] << item if item.length > 0
+  end
+end
+
+def perform_move(stacks, line)
+  match_data = MOVE_LINE_REGEX.match line
+  match_data[:amount].to_i.times do
+    source = match_data[:from].to_i - 1
+    destination = match_data[:to].to_i - 1
+    item = stacks[source].shift
+    stacks[destination].unshift(item)
+  end
+end
+
+InputReader.each_line do |line|
+  build_stacks(stacks, line) if stack_line?(line)
+  perform_move(stacks, line) if move_line?(line)
+end
+
+stacks.each do |stack|
+  print stack[0]
+end
+puts
