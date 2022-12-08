679371df8ebccc8a32d6045ad5b63732d0ad19ed
Ruy Diaz <diazruy@gmail.com>
2022-12-07 22:16:19 -0800

    Add answer to Day 4, part 2

--- main.rb
+++ main.rb
@@ -5,15 +5,15 @@ def to_range(string)
   (min.to_i..max.to_i)
 end
 
-def contained?(range_1, range_2)
-  range_1.cover?(range_2) || range_2.cover?(range_1)
+def overlap?(range_1, range_2)
+  (range_1.to_a & range_2.to_a).length > 0
 end
 
 total = 0
 
 InputReader.each_line do |line|
   ranges = line.split(',').collect { |range_string| to_range(range_string) }
-  total += 1 if contained?(*ranges)
+  total += 1 if overlap?(*ranges)
 end
 
 puts total
