a7d19eeee739de83cb9d3870c43644f9957294f8
Ruy Diaz <diazruy@gmail.com>
2022-12-07 23:29:54 -0800

    Fix InputReader to preserve leading whitespace

--- input_reader.rb
+++ input_reader.rb
@@ -2,7 +2,7 @@ class InputReader
   def self.each_line(file_name = 'input.txt', &block)
     File.open(file_name, 'r') do |file|
       file.each do |line|
-        yield line.strip
+        yield line.chomp
       end
     end
   end
