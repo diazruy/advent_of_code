6d9d18a1e1c58e7a267009a82beba71bf2b6cbff
Ruy Diaz <diazruy@gmail.com>
2022-12-15 22:38:39 -0800

    Add answer to Day 10, part 1

--- main.rb
+++ main.rb
@@ -1,5 +1,31 @@
 require './input_reader'
+require 'byebug'
 
-InputReader.each_line do |line|
-  puts line
+cycle = 1
+register_x = 1
+
+strengths = []
+
+def key_cycle?(cycle)
+  ((cycle + 20) % 40).zero?
 end
+
+def signal_strength(x, cycle)
+  key_cycle?(cycle) ? x * cycle : 0
+end
+
+InputReader.each_line(ARGV[0] || 'input.txt') do |line|
+  command, increment = line.split(' ')
+  increment = increment.to_i
+
+  strengths << signal_strength(register_x, cycle)
+  cycle += 1
+
+  if command ==  'addx'
+    strengths << signal_strength(register_x, cycle)
+    cycle += 1
+    register_x += increment
+  end
+end
+
+puts strengths.sum
