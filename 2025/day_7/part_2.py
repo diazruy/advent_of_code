eceee2719a7dc331b6dbd9348f56abed833ce5ed
Ruy Diaz <diazruy@gmail.com>
2025-12-18 11:09:22 -0800

    Add Day 8 Part 1

---
+++ 2025/day_7/part_2.py
@@ -0,0 +1,26 @@
+import sys
+import re
+file = sys.argv[1]
+
+beams = set()
+splits = 0
+timelines = 0
+
+with open(file + '.txt', 'r') as file:
+    for line in file:
+        if 'S' in line:
+            index = line.index('S')
+            beams.add(index)
+        else:
+            if '^' in line:
+                matches = re.finditer(r"\^", line.strip())
+                for match in matches:
+                    match_index = match.start()
+                    if match_index in beams:
+                        splits += 1
+                        beams.remove(match_index)
+                        beams.add(match_index - 1)
+                        beams.add(match_index + 1)
+                timelines += len(beams)
+print(timelines)
+
